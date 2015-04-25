130936ead9b28b547e29a8d2677c9eb9a8d0280f
shyjal <shyjal99@gmail.com>
2015-04-26 00:39:00 +0530

    interest selection

--- app/app.js
+++ app/app.js
@@ -1,12 +1,48 @@
-L.mapbox.accessToken = 'pk.eyJ1Ijoic2h5amFsIiwiYSI6ImpKbDdtdkUifQ.8Re_EhWxVdbDtxQLPKcgEw';
-var map = L.mapbox.map('map', 'examples.map-i86nkdio')
-    .setView([12.933637,77.614497], 15);
+var Map = function(L) {
+    var that = this;
+    this.init=function(){
+        L.mapbox.accessToken = 'pk.eyJ1Ijoic2h5amFsIiwiYSI6ImpKbDdtdkUifQ.8Re_EhWxVdbDtxQLPKcgEw';
+        window.map = L.mapbox.map('map', 'examples.map-i86nkdio').setView([23.0000, 83.0000], 5);
+        that.bindEvents();
+    }
 
-overlayPane = {
-  "Endpoints" : endpointMarkerLayer,
-  "Links" : linkLineLayer,
-};
+    this.bindEvents = function() {
+        $('.use').click(function() {
+            $('#interest_box').fadeOut(function() {
+                $('#interest_box .row').html('<div class="col s4 shwmap"><h1>HOUSE</h1></div><div class="col s4 shwmap"><h1>AGRI</h1></div><div class="col s4 shwmap"><h1>INDUSTRY</h1></div>');
+                $('#interest_box').fadeIn();
+            });
+        });
+        $('.shwmap').click(function() {
+            $('#interest_box').fadeOut(function() {
+                that.start();
+            });
+        });
 
-// Add a layer control element to the map
-layerControl = L.control.layers(null, overlayPane, {position: 'topright'});
-layerControl.addTo(map);
+    }
+    this.onEachFeature = function(feature, layer) {
+/*
+        layer.bindPopup(popupContent);*/
+    }
+
+    this.start = function() {
+
+        L.geoJson(karnataka, {
+
+            style: function(feature) {
+                return {
+                    color: "#000",
+                    "weight": 1,
+                    "opacity": 0.2,
+                    "dashArray": "3"
+                };
+            },
+
+            onEachFeature: this.onEachFeature,
+
+        }).addTo(map);
+    }
+}
+
+var mapObj=new Map(L);
+mapObj.init();
