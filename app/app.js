5dbf2201801d39b5a08756ec1b300184b7bbaf1d
shyjal <shyjal99@gmail.com>
2015-04-26 12:06:07 +0530

    final ui

--- app/app.js
+++ app/app.js
@@ -55,7 +55,6 @@ var App = function() {
                 } else if (hash == "map-layer") {
                     map.fitBounds(layerSelected.getBounds());
                     that.showData();
-                    that.showAmenities();
 
                 } else {
 
@@ -163,55 +162,14 @@ var App = function() {
     }
     this.showData = function() {
         $('.data_class').fadeIn(function() {
-            Materialize.showStaggeredList('#data-list');
         });
+        Materialize.showStaggeredList('#data-list');
+
 
     }
 
-    this.showAmenities=function() {
-        $('#amenities').fadeIn(function(){
-            Materialize.showStaggeredList('#amenities ul');
-        });
-
-        $( "#amenities li" ).hover(
-          function() {
-            $(this).find('.info').fadeIn(200);
-          }, function() {
-            $(this).find('.info').hide();
-          }
-        );
-    }
 }
 
 var mapApp = new App();
 mapApp.init(L);
 mapApp.bindStates(window);
-
-var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
-
-var barChartData = {
-        labels : ["January","February","March","April","May","June","July"],
-        datasets : [
-            {
-                fillColor : "rgba(220,220,220,0.5)",
-                strokeColor : "rgba(220,220,220,0.8)",
-                highlightFill: "rgba(220,220,220,0.75)",
-                highlightStroke: "rgba(220,220,220,1)",
-                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-            },
-            {
-                fillColor : "rgba(151,187,205,0.5)",
-                strokeColor : "rgba(151,187,205,0.8)",
-                highlightFill : "rgba(151,187,205,0.75)",
-                highlightStroke : "rgba(151,187,205,1)",
-                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-            }
-        ]
-
-}
-window.onload = function(){
-        var ctx = document.getElementById("mapcanvas").getContext("2d");
-        window.myBar = new Chart(ctx).Bar(barChartData, {
-            responsive : true
-        });
-    }
